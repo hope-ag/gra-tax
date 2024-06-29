7f149a9dcef079a2d3a59b5bd736a2cf0e8b8ab5
Hope <62467666+hope-ag@users.noreply.github.com>
2024-06-29 18:14:48 +0000

    added constants

---
+++ src/lib/data/constants.ts
@@ -0,0 +1,48 @@
+export const GRA_TAX_RATES = [
+  {
+    limit: 490,
+    rate: 0
+  },
+  {
+    limit: 110,
+    rate: 0.055
+  },
+  {
+    limit: 130,
+    rate: 0.1
+  },
+  {
+    limit: 3_166.67,
+    rate: 0.175
+  },
+  {
+    limit: 16_000,
+    rate: 0.25
+  },
+  {
+    limit: 30_520,
+    rate: 0.3
+  },
+  {
+    limit: Infinity,
+    rate: 0.35
+  }
+]
+
+export const PENSION_RATES = [
+  {
+    name: 'Tier 1',
+    employer: 0.13,
+    employee: 0
+  },
+  {
+    name: 'Tier 2',
+    employer: 0,
+    employee: 0.055
+  },
+  {
+    name: 'Tier 3',
+    employer: 0.05,
+    employee: 0.05
+  }
+]
